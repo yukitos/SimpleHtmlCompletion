a557c9e1415b8c53c342621469848120892aec31
yukitos <yukitos@gmail.com>
2015-08-02 04:13:30 +0900

    Support multi tags insertion.

--- SimpleHtmlCompletionVSIX/SimpleHtmlCompletion.cs
+++ SimpleHtmlCompletionVSIX/SimpleHtmlCompletion.cs
@@ -35,28 +35,11 @@ namespace SimpleHtmlCompletionVSIX
 
             m_compList = new List<Completion>();
 
-            string element = null;
-            string attrName = null;
-            string attrValue = null;
-            if (text.IndexOf('#') > -1)
-            {
-                var values = text.Split(new char[] { '#' }, 2);
-                element = values[0];
-                attrName = "id";
-                attrValue = values[1];
+            var output = BuildElements(text);
 
-            }
-            else if (text.IndexOf('.') > -1)
+            if (!string.IsNullOrWhiteSpace(output))
             {
-                var values = text.Split(new char[] { '.' }, 2);
-                element = values[0];
-                attrName = "class";
-                attrValue = values[1];
-            }
-            if (!string.IsNullOrWhiteSpace(element))
-            {
-                m_compList.Add(new Completion(
-                    text, string.Format("<{0} {1}=\"{2}\"></{0}>", element, attrName, attrValue), text, null, null));
+                m_compList.Add(new Completion(text, output, text, null, null));
             }
 
             var completionSet = new CompletionSet(
@@ -68,14 +51,52 @@ namespace SimpleHtmlCompletionVSIX
             completionSets.Add(completionSet);
         }
 
+        private string BuildElements(string value)
+        {
+            var values = value.Split(new char[] { '/' }, 2);
+            var firstValue = values[0];
+
+            if (string.IsNullOrWhiteSpace(firstValue))
+                return string.Empty;
+
+            string element = null;
+            string attrName = null;
+            string attrValue = null;
+            if (firstValue.IndexOf('#') > -1)
+            {
+                var xs = firstValue.Split(new char[] { '#' }, 2);
+                element = xs[0];
+                attrName = "id";
+                attrValue = xs[1];
+
+            }
+            else if (firstValue.IndexOf('.') > -1)
+            {
+                var xs = firstValue.Split(new char[] { '.' }, 2);
+                element = xs[0];
+                attrName = "class";
+                attrValue = xs[1];
+            }
+
+            var remaining = values.Length > 1 ? values[1] : string.Empty;
+            return string.Format("<{0} {1}=\"{2}\">{3}</{0}>",
+                element, attrName, attrValue, BuildElements(remaining));
+        }
+
         private ITrackingSpan FindTokenSpanAtPosition(ITrackingPoint point, ICompletionSession session)
         {
             SnapshotPoint currentPoint = (session.TextView.Caret.Position.BufferPosition) - 1;
             ITextStructureNavigator navigator = m_sourceProvider.NavigatorService.GetTextStructureNavigator(m_textBuffer);
-            TextExtent firstExtent = navigator.GetExtentOfWord(currentPoint); // text span of id or class
-            var secondExtent = navigator.GetExtentOfWord(firstExtent.Span.Start - 1); // delimiter ('#' or '.')
-            var thirdExtent = navigator.GetExtentOfWord(secondExtent.Span.Start - 1); // text span of element's name
-            var targetSpan = new SnapshotSpan(thirdExtent.Span.Start, firstExtent.Span.End);
+
+            // Extend the span until the first whitespace char occurs beforehand.
+            var currentExtent = navigator.GetExtentOfWord(currentPoint);
+            var lastPosition = currentExtent.Span.End;
+            while (!char.IsWhiteSpace(currentExtent.Span.Start.GetChar()))
+            {
+                currentExtent = navigator.GetExtentOfWord(currentExtent.Span.Start - 1);
+            }
+
+            var targetSpan = new SnapshotSpan(currentExtent.Span.End, lastPosition);
             return currentPoint.Snapshot.CreateTrackingSpan(targetSpan, SpanTrackingMode.EdgeInclusive);
         }
 
@@ -156,21 +177,39 @@ namespace SimpleHtmlCompletionVSIX
             if (nCmdID == (uint)VSConstants.VSStd2KCmdID.TAB)
             {
                 TriggerCompletion();
-                var targetCompletionSet = m_session.CompletionSets.FirstOrDefault(i => i.GetType() == typeof(CompletionSet));
-                if (targetCompletionSet != null)
+                var targetCompletionSets = m_session.CompletionSets.Where(i => i.GetType() == typeof(CompletionSet));
+                foreach (var targetCompletionSet in targetCompletionSets)
                 {
-                    m_session.SelectedCompletionSet = targetCompletionSet;
-                    m_session.SelectedCompletionSet.SelectBestMatch();
-                    if (m_session.SelectedCompletionSet.SelectionStatus.IsSelected)
+                    targetCompletionSet.SelectBestMatch();
+                    if (targetCompletionSet.SelectionStatus.IsSelected && targetCompletionSet.SelectionStatus.IsUnique)
                     {
-                        m_session.Commit();
-                        return VSConstants.S_OK;
+                        var insertedText = targetCompletionSet.Completions.First().InsertionText;
+                        if (insertedText.StartsWith("<"))
+                        {
+                            m_session.SelectedCompletionSet = targetCompletionSet;
+                            m_session.Commit();
+
+                            var targetIndex = insertedText.LastIndexOf("</");
+                            while (targetIndex > -1)
+                            {
+                                var index = insertedText.LastIndexOf("</", targetIndex);
+                                if (index == -1)
+                                    break;
+
+                                targetIndex = index;
+                            }
+
+                            // move the caret in between the tags
+                            for (var i = 0; i < (insertedText.Length - targetIndex); i++)
+                            {
+                                m_textView.Caret.MoveToPreviousCaretPosition();
+                            }
+                            return VSConstants.S_OK;
+                        }
                     }
                 }
-                else
-                {
-                    m_session.Dismiss();
-                }
+
+                m_session.Dismiss();
             }
 
             return m_nextCommandHandler.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
